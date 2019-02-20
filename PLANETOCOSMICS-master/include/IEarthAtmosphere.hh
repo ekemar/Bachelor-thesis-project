4ec62a4852e74bf2f78386e2942e6a10d1dee5a0
Liselott <lekemar@kth.se>
2019-02-20 19:58:22 +0100

    "Deleted planetocosmics"

--- PLANETOCOSMICS-master/include/IEarthAtmosphere.hh
+++
@@ -1,6 +0,0 @@
-extern "C"
-{extern void gtd6_(int*, float*, float*, float*, float*,
-        float*,  float*,  float*,  float*, int*, float*, float*);
- extern void gtd7_(int*, float*, float*, float*, float*,
-        float*,  float*,  float*,  float*, int*, float*, float*);
-}
